905678d403c07f2a3ba7a2ccce21f453c14f3b42
Mason Allen <friskspacifism@gmail.com>
2024-01-13 18:33:44 -0700

    Prep excersizes 1 and 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //prompt user for their first and last names
+        Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+
+        //repeat it back to them
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //prompt the user for their grade percentage
+        Console.Write("Please enter your grade percentage: ");
+        string grade = Console.ReadLine();
+        //parse the given string for an int
+        int percentage = int.Parse(grade);
+
+        string letter = "X";
+
+        //I know the information was about if and else, but it's better handled by a switch case in my opinion. It's less bulky, and easier to read.
+        switch(percentage){
+            case >= 90:
+                letter = "A";
+                break;
+            case >= 80:
+                letter = "B";
+                break;
+            case >= 70:
+                letter = "C";
+                break;
+            case >= 60:
+                letter = "D";
+                break;
+            default:
+                letter = "F";
+                break;
+        }
+        //display results
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You pass this class!");
+        }
+        else
+        {
+            Console.WriteLine("You fail this class. Try again!");
+        }
+        Console.WriteLine($"The grade you earned: {letter}");
     }
 }
