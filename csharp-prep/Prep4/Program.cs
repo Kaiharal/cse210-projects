492feb1bf80cc838ed2cc1f9d90c8c09bd4e5971
Mason Allen <friskspacifism@gmail.com>
2024-01-20 21:09:18 -0700

    Prep 4 Finished

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numList = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int userInput = 1;
+        int counter = 0;
+        //getting the list
+        while(userInput != 0){
+            Console.Write("Enter number: ");
+            userInput = Convert.ToInt32(Console.ReadLine());
+            if(userInput == 0){
+                break;
+            }
+            numList.Add(userInput);
+            counter++;
+        }
+        //calculations
+        int total = 0;
+        int largest = 0;
+        foreach(int num in numList){
+            total += num;
+            if(num > largest){
+                largest = num;
+            }
+        }
+        float average = total / counter;
+        Console.WriteLine("The sum is " + total);
+        Console.WriteLine("The average is " + average);
+        Console.WriteLine("The largest number is " + largest);
     }
 }
