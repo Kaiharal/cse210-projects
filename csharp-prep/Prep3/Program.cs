47036dcadc28313c825203f8fec8f25f2da6a193
Mason Allen <friskspacifism@gmail.com>
2024-01-20 19:19:58 -0700

    Actually uploading Prep 3 this time

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Guess a number between 0 and 100!");
+        Random rnd = new Random();
+        int num = rnd.Next(100);
+
+        bool guessed = false;    
+        int guess;
+        while(!guessed){
+            guess = Convert.ToInt32(Console.ReadLine());
+            if(guess < num){
+                Console.WriteLine("Too Low!");
+            }
+            if(guess > num){
+                Console.WriteLine("Too High!");
+            }
+            if(guess == num){
+                guessed = true;
+            }    
+        }
+        Console.WriteLine("Correct!");
     }
 }
